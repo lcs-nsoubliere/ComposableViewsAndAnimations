77f05dc3fb3d2720b3e968e29b7287a4a57460e7
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-04-26 09:24:39 -0400

    Added in a rotating circle animation that bounces from left side of screen to right

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    //MARK: Stored Properties
+    //horizontal position
+    @State var xOffset = -100.0
+    
+    //Rotation Amount
+    @State var rotationAmount = 0.0
+    
+    let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
+  
+    //MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                .frame(width: 50, height: 50)
+               
+            
+            Text("OK")
+                .foregroundColor(.white)
+        }
+        .offset(x: xOffset, y: 0)
+        .rotationEffect(.degrees(Double(rotationAmount)),anchor: .center)
+        
+        .animation(
+            Animation
+                .easeInOut(duration: 2)
+                    .repeatForever(autoreverses: true)
+        )
+        
+        .onReceive(timer) { input in
+            //move the circle and text to the right
+            xOffset = 100.0
+            //turn once
+            rotationAmount = 360.0
+            
+            //Turn off the timer
+            timer.upstream.connect().cancel()
+        }
     }
 }
 
